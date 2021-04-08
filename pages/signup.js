150b21dbc6e0fb4652ba1470e3ddc48f7265957e
Vitor Caetano <vitorlc95@gmail.com>
2021-04-08 15:41:49 -0300

    feat: login and signup with firebase auth

--- pages/signup.js
+++ pages/signup.js
@@ -6,6 +6,8 @@ import {
   Container,
   Box,
   Input,
+  InputGroup,
+  InputLeftAddon,
   Button,
   Text,
   FormControl,
@@ -19,6 +21,7 @@ import firebase from '../config/firebase'
 const validationSchema = yup.object().shape({
   email: yup.string().email('Email inválido').required('Preenchimento obrigatório'),
   password: yup.string().required('Preenchimento obrigatório'),
+  username: yup.string().required('Preenchimento obrigatório'),
 });
 
 export default function Home() {
@@ -32,7 +35,7 @@ export default function Home() {
     isSubmitting
   } = useFormik({
     onSubmit: async (values, form) => {
-      const user = await firebase.auth().signInWithEmailAndPassword(values.email, values.password)
+      const user = await firebase.auth().createUserWithEmailAndPassword(values.email, values.password)
       console.log(user)
     },
     validationSchema,
@@ -63,13 +66,21 @@ export default function Home() {
           {touched.password && <FormHelperText textColor="#e74c3c">{errors.password}</FormHelperText>}
         </FormControl>
 
+
+        <FormControl id="username" p={4} isRequired>
+          <InputGroup size="lg">
+            <InputLeftAddon children="clocker.work/" />
+            <Input type="username" value={values.username} onChange={handleChange} onBlur={handleBlur} />
+          </InputGroup>
+          {touched.username && <FormHelperText textColor="#e74c3c">{errors.username}</FormHelperText>}
+        </FormControl>
+
         <Box p={4}>
           <Button colorScheme="blue" width="100%" onClick={handleSubmit} isLoading={isSubmitting}>Entrar</Button>
         </Box>
       </Box>
 
-      <Link href="/signup">Ainda não tem uma conta? Cadastre-se</Link>
+      <Link href="/">Já tem uma conta? Acesse</Link>
     </Container>
-
   )
 }
